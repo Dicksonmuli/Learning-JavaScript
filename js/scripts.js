b0e300569c81d339f03534e7c81a261dd606074f
dicksonmuli <dicksonmully6@gmail.com>
2017-02-15 11:24:07 +0300

    Initial learning-javascript commit

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+$("h1").click(function(){
+    alert("this is a header.");
+});
+
+$("p").click(function(){
+    alert("This is a paragraph.");
+});
+$("img").click(function(){
+    alert("this is an image");
+});
